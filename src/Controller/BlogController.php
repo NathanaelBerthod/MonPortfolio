c99cdec49e00c028c5139430494fd672b77cf467
NathanaelBerthod <nathanael.berthod@etu.univ-st-etienne.fr>
2025-12-07 21:53:25 +0100

    propre

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -11,8 +11,24 @@ final class BlogController extends AbstractController
     #[Route('/blog', name: 'app_blog')]
     public function index(): Response
     {
-        return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
-        ]);
+        return $this->render('blog/index.html.twig');
+    }
+
+    #[Route('/hayao-miyazaki', name: 'page1')]
+    public function page1(): Response
+    {
+        return $this->render('blog/page1.html.twig');
+    }
+
+    #[Route('/isao-takahata', name: 'page2')]
+    public function page2(): Response
+    {
+        return $this->render('blog/page2.html.twig');
+    }
+
+    #[Route('/contact', name: 'page3')]
+    public function page3(): Response
+    {
+        return $this->render('blog/page3.html.twig');
     }
 }
