1329a0161db3a7549e22011d4494d8ec0858f042
Berthod Nathanael <berthod.nathanael@gmail.com>
2025-12-31 15:38:54 +0100

    Enregistrer

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,10 +4,17 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class BlogController extends AbstractController
 {
+    #[Route('/', name: 'app_root')]
+    public function root(): RedirectResponse
+    {
+        return $this->redirect('/blog');
+    }
+
     #[Route('/blog', name: 'app_blog')]
     public function index(): Response
     {
